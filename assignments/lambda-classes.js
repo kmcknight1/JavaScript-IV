430ba592adb9bc8986ce7ca85854451203eb2b06
kmcknight1 <kmcknight1991@gmail.com>
2019-05-09 22:44:36 -0500

    MVP and stretch complete

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,222 @@
-// CODE here for your Lambda Classes
+class Person {
+  constructor(attr) {
+    this.name = attr.name;
+    this.age = attr.age;
+    this.location = attr.location;
+  }
+
+  speak() {
+    return `Hello, my name is ${this.name} and I am from ${this.location}.`;
+  }
+}
+
+class Instructor extends Person {
+  constructor(attr) {
+    super(attr);
+    this.specialty = attr.specialty;
+    this.favLanguage = attr.favLanguage;
+    this.catchPhrase = attr.catchPhrase;
+  }
+
+  demo(subject) {
+    return `Today we are learning about ${subject}.`;
+  }
+
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}.`;
+  }
+
+  affectGrade(student) {
+    Math.random() < 0.5
+      ? (student.grade -= Math.round(Math.random()))
+      : (student.grade += Math.round(Math.random() * 15));
+  }
+}
+
+class Student extends Person {
+  constructor(attr) {
+    super(attr);
+    this.previousBackground = attr.previousBackground;
+    this.className = attr.className;
+    this.favSubjects = attr.favSubjects;
+    this.grade = attr.grade;
+  }
+
+  listsSubjects() {
+    return this.favSubjects.toString().split(",");
+  }
+
+  PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}.`;
+  }
+
+  sprintChallenge(subject) {
+    return `${this.name} has begun the sprint challenge on ${subject}.`;
+  }
+
+  graduateOrNah() {
+    return this.grade >= 70 ? (this.grade = "Graduated!") : "Keep trying!";
+  }
+}
+
+class ProjectManager extends Instructor {
+  constructor(attr) {
+    super(attr);
+    this.gradClassName = attr.gradClassName;
+    this.favInstructor = attr.favInstructor;
+  }
+
+  standUp(channel) {
+    return `${this.name} announces to ${channel} @channel standby times!`;
+  }
+
+  debugsCode(student, subject) {
+    return `${this.name} debugs ${student.name}'s code on ${subject}.`;
+  }
+}
+
+const dan = new Instructor({
+  name: "Dan Levy",
+  age: 40,
+  location: "Cat Land",
+  specialty: "Cat Jokes",
+  favLanguage: "JavaScript",
+  catchPhrase: "*nervous laughter*"
+});
+
+const josh = new Instructor({
+  name: "Josh Knell",
+  age: 40,
+  location: "Salt Lake City",
+  gender: "M",
+  specialty: "Vanilla JavaScript",
+  favLanguage: "JavaScript",
+  catchPhrase: "Ha, ha, ha.. oh boy."
+});
+
+const joshua = new ProjectManager({
+  name: "Joshua Wood",
+  age: 40,
+  location: "somewhere west, USA",
+  specialty: "being a kind and helpful person",
+  favLanguage: "React JS",
+  catchPhrase: "You're doing great.",
+  gradClassName: "Web20",
+  favInstructor: "Josh Knell"
+});
+
+const chance = new ProjectManager({
+  name: "Chance Halo",
+  age: 22,
+  location: "St. Joseph, MO",
+  gender: "M",
+  specialty: "Farting",
+  favLanguage: "React JS",
+  catchPhrase: "I'm a big turd.",
+  gradClassName: "Labs12",
+  favInstructor: "Dustin"
+});
+
+const katie = new Student({
+  name: "Katie McKnight",
+  age: 27,
+  location: "St. Joseph, MO",
+  previousBackground: "Behavioral Health",
+  className: "Web20",
+  favSubjects: ["JavaScript", "Literature", "Psychology", "Economics"],
+  grade: 0
+});
+
+const chris = new Student({
+  name: "Chris Pelayo",
+  age: 23,
+  location: "Los Angeles, CA",
+  prevBackground: "Sales",
+  className: "Web20",
+  favSubjects: ["JavaScript", "CSS", "HTML"],
+  grade: 0
+});
+
+const winnie = new Student({
+  name: "Winnie Song",
+  location: "FLushing",
+  age: 22,
+  previousBackground: `MUA`,
+  className: `Web20`,
+  favSubjects: ["CSS", "HTML", "LESS"],
+  grade: 0
+});
+
+const micah = new Student({
+  name: "Micah",
+  location: "Provo",
+  age: 23,
+  previousBackground: "Office Manager",
+  className: "Web 20",
+  favSubjects: ["CSS", "JS", "HTML"],
+  grade: 0
+});
+
+const ramses = new Student({
+  name: "Ramses Bermudez",
+  age: 25,
+  location: "Miami, FL",
+  prevBackground: "Real Estate",
+  className: "Web20",
+  favSub: ["CSS", "HTML", "JS"],
+  grade: 0
+});
+
+console.table(dan);
+console.table(josh);
+console.table(joshua);
+console.log(katie);
+console.table(chance);
+
+console.log(katie.speak());
+console.log(dan.demo("constructor functions"));
+console.log(dan.grade(katie, "JavaScript III"));
+
+console.log(joshua.standUp("Web20"));
+console.log(joshua.debugsCode(katie, "DOM manipulation"));
+
+console.log(katie.listsSubjects());
+console.log(katie.PRAssignment("JavaScript IV"));
+console.log(katie.sprintChallenge("DOM I"));
+
+console.log(katie.grade);
+console.log(dan.affectGrade(katie));
+console.log(katie.grade);
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(katie.grade);
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(katie.grade);
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(katie.grade);
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(dan.affectGrade(katie));
+console.log(katie.grade);
+console.log(katie.graduateOrNah());
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -1,9 +1,167 @@
-/* 
+class GameObject {
+  constructor(attrs) {
+    this.createdAt = attrs.createdAt;
+    this.name = attrs.name;
+    this.dimensions = attrs.dimensions;
+  }
 
-Prototype Refactor
+  destroy() {
+    return `${this.name} was removed from the game.`;
+  }
+}
 
-1. Copy and paste your code or the solution from yesterday
+class CharacterStats extends GameObject {
+  constructor(charAttrs) {
+    super(charAttrs);
+    this.healthPoints = charAttrs.healthPoints;
+  }
 
-2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
+  takeDamage() {
+    return `${this.name} took damage.`;
+  }
+}
 
-*/
+class Humanoid extends CharacterStats {
+  constructor(humAttrs) {
+    super(humAttrs);
+    this.team = humAttrs.team;
+    this.weapons = humAttrs.weapons;
+    this.language = humAttrs.language;
+  }
+
+  greet() {
+    return `${this.name} offers a greeting in ${this.language}.`;
+  }
+}
+
+class Hero extends Humanoid {
+  constructor(heroAttrs) {
+    super(heroAttrs);
+    this.specialPower = heroAttrs.specialPower;
+  }
+
+  heal(char, x) {
+    char.healthPoints += x;
+
+    if (x < 2) {
+      return `${this.name} used ${this.specialPower} to heal ${
+        char.name
+      } by ${x} point.`;
+    } else {
+      return `${this.name} used ${this.specialPower} to heal ${
+        char.name
+      } by ${x} points.`;
+    }
+  }
+}
+
+class Villian extends Humanoid {
+  constructor(villAttrs) {
+    super(villAttrs);
+    this.evilSaying = villAttrs.evilSaying;
+  }
+
+  attack(char, x) {
+    if (x > char.healthPoints) {
+      char.healthPoints = "Dead";
+      return `${this.name} has killed ${char.name}.`;
+    } else {
+      char.healthPoints -= x;
+      return `${this.name} has taken ${x} health from ${char.name}.`;
+    }
+  }
+}
+
+const mage = new Humanoid({
+  createdAt: new Date(),
+  name: "Bruce",
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1
+  },
+  healthPoints: 5,
+  team: "Mage Guild",
+  weapons: ["Staff of Shamalama"],
+  language: "Common Tongue"
+});
+
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  name: "Sir Mustachio",
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2
+  },
+  healthPoints: 15,
+  team: "The Round Table",
+  weapons: ["Giant Sword", "Shield"],
+  language: "Common Tongue"
+});
+
+const archer = new Humanoid({
+  createdAt: new Date(),
+  name: "Lilith",
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4
+  },
+  healthPoints: 10,
+  team: "Forest Kingdom",
+  weapons: ["Bow", "Dagger"],
+  language: "Elvish"
+});
+
+const wizard = new Hero({
+  createdAt: new Date(),
+  name: "Remus",
+  dimensions: {
+    length: 3,
+    width: 3,
+    height: 5
+  },
+  healthPoints: 35,
+  team: "Dumbledore's Army",
+  weapons: ["Wand", "Cloak of Invisibility", "Pensieve"],
+  language: "Common Tongue",
+  specialPower: "Magic"
+});
+
+const politician = new Villian({
+  createdAt: new Date(),
+  name: "Herman",
+  dimensions: {
+    length: 2,
+    width: 5,
+    height: 2
+  },
+  healthPoints: 30,
+  team: "Republican",
+  weapons: ["Propoganda", "Fox News", "Rich Family"],
+  language: "Filibuster",
+  evilSaying: "We deserve stuff, they don't!"
+});
+
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+console.log(politician.attack(mage, 3));
+console.log(wizard.heal(mage, 4));
+console.log(politician.attack(mage, 6));
+console.log(wizard.heal(mage, 2));
+console.log(politician.attack(mage, 3));
+
+console.log(mage.healthPoints);
+
+console.log(wizard.heal(archer, 3));
+console.log(archer.healthPoints);
